5d749a7df2c1b75a92a0a54ea55856965d8af04f
Erivks <mordecai.es27@gmail.com>
2020-05-11 20:41:03 -0300

    Rotas para paciente

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/pacientes', 'PacienteController@storeAJAX')->name('api.paciente');
+Route::post('/fisio', function () {
+    return 'oi';
+});
