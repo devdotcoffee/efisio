61a0302c79b4f24e38dbcd7af6ff602545f2160f
Erick Oliveira dos Santos <eoliveira@previni-ni.local>
2020-05-14 15:10:30 -0300

    Controller e Model de Fisio

--- app/Fisio.php
+++ app/Fisio.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fisio extends Model
 {
-    //
+    protected $table = 'fisioterapeutas';
+
+    public static function todos()
+    {
+        return self::all();
+    }
+
+    public static function salvar($request)
+    {
+        $fisio = new Fisio();
+        $fisio->nome = $request->input('fisioNome');
+        $fisio->data_nascimento = $request->input('fisioNascimento');
+        $fisio->crefito = $request->input('fisioCrefito');
+        $fisio->cpf = $request->input('fisioCpf');
+        $fisio->save();
+
+        return $fisio;
+    }
+
+    public static function getFisioById($id)
+    {
+        return Fisio::where('idFisioterapeuta', $id)
+            ->first();
+    }
+
+    public static function alterar($id, $request)
+    {
+        Fisio::where('idFisioterapeuta', $id)
+            ->update([
+                'nome'              => $request->input('fisioNome'),
+                'data_nascimento'   => $request->input('fisioNascimento'),
+                'crefito'           => $request->input('fisioCrefito'),
+                'cpf'               => $request->input('fisioCpf')
+            ]);
+    }
 }
