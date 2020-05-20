807f5e8380bec29b4e72f68b906e3e8292221e9d
Erivks <mordecai.es27@gmail.com>
2020-05-20 17:30:18 -0300

    Alinhando imagem

--- resources/views/fisio/home.blade.php
+++ resources/views/fisio/home.blade.php
@@ -8,7 +8,9 @@
             <div class="card-body">
                 <h3 class="card-title text-center">Bem vindo ao e-fisio</h3>
                 <p class="text-center">O e-fisio é um sistema que possibilita o cadastro e gerenciamento de dados como: pacientes, prontuários e fisioterapêutas. Com o objetivo de controlar com segurança o acesso a dados e facilitar a consulta dos mesmos.</p>
-                <img src="{{ asset('img/logo-devdot.png') }}" alt="Logo DevDotCoffee">
+                <a href="https://github.com/devdotcoffee">
+                    <img class="mx-auto d-block" src="{{ asset('img/logo-devdot.png') }}" alt="Logo DevDotCoffee">
+                </a>
             </div>
         </div>
     </div>
