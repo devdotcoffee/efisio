bafd84b7d767d070f3e43d9b2eb339fa43d87b5d
Erick Oliveira dos Santos <eoliveira@previni-ni.local>
2020-05-25 15:24:55 -0300

    Controller para gerar pdf

---
+++ app/Http/Controllers/PdfController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Prontuario;
+use App\Evolucao;
+use PDF;
+
+class PdfController extends Controller
+{
+    public function builderProntuario($id)
+    {
+        $prontuario = Prontuario::getProntuarioById($id);
+        $evolucoes = Evolucao::getEvolucaoByProntuario($id);
+
+        $pdf = PDF::loadView('pdf.prontuario', ['prontuario' => $prontuario, 'evolucoes' => $evolucoes]);
+        
+        return $pdf->setPaper('a4')->stream('prontuario.pdf');
+    }
+
+}
