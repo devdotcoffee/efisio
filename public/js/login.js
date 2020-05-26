b56aebb0d6dba0038b226b410c8bfe76072eca5b
Erick Oliveira dos Santos <eoliveira@previni-ni.local>
2020-05-26 15:24:33 -0300

    Usando timeout para mensagem de erro desaparecer

--- public/js/login.js
+++ public/js/login.js
@@ -4,3 +4,8 @@ $('#collapseFisio').on('show.bs.collapse', () => {
 $('#collapsePaciente').on('show.bs.collapse', () => {
     $('#collapseFisio').collapse('hide');
 });
+$(document).ready(function(){
+    setTimeout(function() {
+        $(".alert").alert('close');
+    }, 3000);
+});
